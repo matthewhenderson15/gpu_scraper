8d288c69224292e17b42f8799a31d0f5b801809e
Matthew Henderson <info@matthewahenderson.com>
2021-11-12 23:22:07 -0500

    Initial scraping file

---
+++ scraper.py
@@ -0,0 +1,20 @@
+import bs4
+from bs4 import BeautifulSoup as bs
+import time
+import requests
+
+links = {"bb_FE":"https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440","bb_Gigabyte":"https://www.bestbuy.com/site/gigabyte-geforce-rtx-3080-10g-gddr6x-pci-express-4-0-graphics-card-black/6430621.p?skuId=6430621"
+         ,"bb_MSI":"https://www.bestbuy.com/site/msi-geforce-rtx-3080-ventus-3x-10g-oc-bv-gddr6x-pci-express-4-0-graphic-card-black-silver/6430175.p?skuId=6430175","bb_EVGA":"https://www.bestbuy.com/site/evga-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card/6432399.p?skuId=6432399"
+         ,"bb_ASUS":"https://www.bestbuy.com/site/asus-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-strix-graphics-card-black/6432445.p?skuId=6432445#tabbed-customerreviews","bb_Gigabyte2":"https://www.bestbuy.com/site/gigabyte-geforce-rtx-3080-10g-gddr6x-pci-express-4-0-graphics-card-black/6430620.p?skuId=6430620"
+         ,"bb_EVGA2":"https://www.bestbuy.com/site/evga-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card/6432400.p?skuId=6432400"}
+
+sold_bb_fe = False
+sold_bb_gigabyte = False
+sold_bb_msi = False
+sold_bb_evga = False
+sold_bb_asus = False
+sold_bb_gigabyte_2 = False
+sold_bb_evga2 = False
+
+def checkEachLink():
+    
